3f1fd5baf0954bb389aa122b15506d2ac76d6fc8
Víctor Andrés Rojas <victandres98@gmail.com>
2024-08-06 11:31:57 -0500

    feat: implement pokemon list view

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:poke_app/providers/providers.dart';
+import 'package:poke_app/widgets/widgets.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +13,23 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Home Screen');
+    final pokemonProvider = Provider.of<PokemonProvider>(context);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Poke App'),
+      ),
+      body: (pokemonProvider.pokemons.isEmpty)
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: pokemonProvider.pokemons.length,
+              itemBuilder: (context, index) {
+                return PokemonCard(
+                  pokemon: pokemonProvider.pokemons[index],
+                );
+              }),
+    );
   }
 }
