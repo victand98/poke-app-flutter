ac47e747cd6ff11317591d8c5b1b2fc8c5dd41c1
Víctor Andrés Rojas <victandres98@gmail.com>
2024-08-06 11:31:57 -0500

    feat: implement pokemon list view

--- lib/providers/pokemon_provider.dart
+++ lib/providers/pokemon_provider.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:poke_app/models/models.dart';
+import 'package:poke_app/services/services.dart';
 
 class PokemonProvider extends ChangeNotifier {
   PokemonProvider() {
     getInitialData();
   }
 
+  final List<Pokemon> _pokemons = [];
+  int pokemonId = 1;
+
   void getInitialData() async {
-    // Fetch data from API
+    for (int i = 1; i <= 10; i++) {
+      final pokemon = await PokemonService.getPokemon(i);
+      _pokemons.add(pokemon);
+      pokemonId++;
+    }
+
     notifyListeners();
   }
+
+  List<Pokemon> get pokemons => _pokemons;
 }
