7f0bd354c85a6c14b5c986ec3b9d4d661160fd4b
Víctor Andrés Rojas <victandres98@gmail.com>
2024-08-06 13:21:09 -0500

    feat: add stats section to details screen

--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,3 +1,7 @@
 String capitalize(String text) {
   return text[0].toUpperCase() + text.substring(1).toLowerCase();
 }
+
+String getAssetImage(String type, String name) {
+  return 'assets/img/${type}_types/$name.png';
+}
