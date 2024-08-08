881355ffd1d968f206d4ace48952a56985a87456
Víctor Andrés Rojas <victandres98@gmail.com>
2024-08-08 10:14:49 -0500

    feat: add view model to list my pokemons captures

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,1 +1,2 @@
 export 'package:poke_app/models/pokemon.dart';
+export 'package:poke_app/models/pokemon_captures.dart';
---
+++ lib/view_models/view_models.dart
@@ -0,0 +1,1 @@
+export 'package:poke_app/view_models/my_pokemons_view_model.dart';
